1e907aed8b2ec9daf2bc321f8ff2ff71237ebf31
Erfan Mansoori <146859920+emansoori@users.noreply.github.com>
2025-04-23 22:31:47 -0230

    Update GradCam.py
    Compute gradients for target class using GradientTape.

--- GradCam.py
+++ GradCam.py
@@ -14,3 +14,11 @@ grad_model = models.Model(
     inputs=[net.inputs],
     outputs=[target_layer.output, net.output]
 )
+
+def compute_gradcam(input_image, model, target_class):
+    print("Input image shape:", input_image.shape)
+    with tf.GradientTape() as tape:
+        conv_outputs, predictions = grad_model(input_image)
+        predictions = tf.convert_to_tensor(predictions)
+        print("Predictions shape:", predictions.shape)
+        loss = predictions[0, target_class]
