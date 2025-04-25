484b9f72106f735cd36d8964f88a66706ceaefdc
Erfan Mansoori <146859920+emansoori@users.noreply.github.com>
2025-04-25 17:16:40 -0230

    Update GradCam.py
    Generate heatmap with smooth resizing

--- GradCam.py
+++ GradCam.py
@@ -49,3 +49,6 @@ img_normalized = np.expand_dims(img_resized / 255.0, axis=0)
 preds = net.predict(img_normalized)
 class_idx = np.argmax(preds[0])
 cam = compute_gradcam(img_normalized, grad_model, class_idx)
+
+heatmap = cv2.applyColorMap(np.uint8(255 * cam), cv2.COLORMAP_JET)
+heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]), interpolation=cv2.INTER_LINEAR)
