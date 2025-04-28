1f27dddf907053ac68567d5307cdff0d660b5968
Erfan Mansoori <146859920+emansoori@users.noreply.github.com>
2025-04-28 20:46:01 -0230

    Create GradCam++.py
    Start Grad-CAM++ implementation: load trained model and setup grad_model to extract feature maps and predictions.

---
+++ GradCam++.py
@@ -0,0 +1,15 @@
+from tensorflow import keras
+from keras import layers, models
+import cv2
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+net = models.load_model('/content/drive/MyDrive/Colab Notebooks/CNN_functional_Train_Test.h5')
+
+target_layer = net.get_layer("block5_conv3")
+
+grad_model = models.Model(
+    inputs=[net.inputs],
+    outputs=[target_layer.output, net.output]
+)
