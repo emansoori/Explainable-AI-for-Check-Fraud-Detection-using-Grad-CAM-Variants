4ac817ff736503364e796dd131492e42d25f791c
Erfan Mansoori <146859920+emansoori@users.noreply.github.com>
2025-05-06 23:36:41 -0230

    Update XGrad-CAM.py
    Load model and define grad_model

--- XGrad-CAM.py
+++ XGrad-CAM.py
@@ -3,3 +3,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import models
 import matplotlib.pyplot as plt
+
+net = models.load_model('/content/drive/MyDrive/Colab Notebooks/CNN_functional_Train_Test.h5')
+target_layer = net.get_layer("block5_conv3")
+grad_model = models.Model(inputs=[net.input], outputs=[target_layer.output, net.output])
