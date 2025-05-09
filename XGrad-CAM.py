f4a155058274722c93d930f0e984e99b19e4460e
Erfan Mansoori <146859920+emansoori@users.noreply.github.com>
2025-05-08 23:47:45 -0230

    Update XGrad-CAM.py
    Pre-process image

--- XGrad-CAM.py
+++ XGrad-CAM.py
@@ -35,3 +35,12 @@ def compute_xgradcam(input_image, model, target_class):
     cam = cv2.resize(cam, (input_image.shape[2], input_image.shape[1]))
     cam /= cam.max() if cam.max() != 0 else 1
     return cam
+
+
+img_path = '/content/drive/MyDrive/Colab Notebooks/Test10.Original.jpg'
+img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+img_resized = cv2.resize(img, (224, 224))
+img_normalized = np.expand_dims(img_resized / 255.0, axis=0)
+
+
